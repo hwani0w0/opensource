bac5f5676469d6c16e0a80759a7ec8054e59f113
hyehwan <hyehwan25@gmail.com>
2023-06-15 11:09:40 +0900

    Create jenkins-with-docker.sh

---
+++ jenkins-with-docker.sh
@@ -0,0 +1,62 @@
+#!/bin/sh
+
+# OS:Ubuntu 20.04
+
+# Env Conf
+sudo hostnamectl set-hostname jenkins
+
+sudo sed -i 's/#PasswordAuthentication yes/PasswordAuthentication yes/' /etc/ssh/sshd_config
+sudo sed -i '/PasswordAuthentication no/ s/^/#/' /etc/ssh/sshd_config
+sudo sed -i '/GSSAPIAuthentication yes/ s/^/#/' /etc/ssh/sshd_config
+sudo systemctl restart sshd
+
+echo "jenkins:cloud123" | sudo chpasswd
+
+echo "jenkins ALL = (ALL) NOPASSWD:ALL" | sudo tee /etc/sudoers.d/jenkins
+sudo chmod 0440 /etc/sudoers.d/jenkins
+
+# AWS CLI install
+curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
+sudo apt install unzip
+sudo unzip awscliv2.zip
+sudo ./aws/install
+export PATH=/usr/local/bin:$PATH
+source ~/.bash_profile
+
+access_key="{aws user access key}"
+secret_access_key="{aws user secret access key}"
+region="ap-northeast-2"
+
+aws configure << EOF
+${access_key}
+${secret_access_key}
+${region}
+json
+EOF
+
+# Docker install
+sudo apt-get install apt-transport-https ca-certificates curl gnupg-agent software-properties-common -y
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io -y
+
+sudo systemctl enable docker
+
+sudo groupadd docker
+sudo usermod -aG docker jenkins
+sudo systemctl restart docker
+
+# jenkins install
+sudo apt-get install openjdk-11-jdk -y
+wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+sudo apt-key adv --keyserver  keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+
+sudo apt-get update
+sudo apt-get install jenkins -y
+
+sudo systemctl enable jenkins
+sudo systemctl start jenkins
+
+sudo cat /var/lib/jenkins/secrets/initialAdminPassword
