3a96b8717cf72f9cbda214120f55fda4d5d94b07
hyehwan <hyehwan25@gmail.com>
2023-06-20 15:48:44 +0900

    Update jenkins-with-docker.sh

--- jenkins-with-docker.sh
+++ jenkins-with-docker.sh
@@ -51,9 +51,14 @@ sudo systemctl restart docker
 
 # jenkins install
 sudo apt-get install openjdk-11-jdk -y
-wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
-sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
-sudo apt-key adv --keyserver  keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+#wget -q -O - https://pkg.jenkins.io/debian-stable/jenkins.io.key |sudo gpg --dearmor -o /usr/share/keyrings/jenkins.gpg
+#sudo sh -c 'echo deb [signed-by=/usr/share/keyrings/jenkins.gpg] http://pkg.jenkins.io/debian-stable binary/ > /etc/apt/sources.list.d/jenkins.list'
+#sudo apt-key adv --keyserver  keyserver.ubuntu.com --recv-keys 5BA31D57EF5975CA
+curl -fsSL https://pkg.jenkins.io/debian/jenkins.io-2023.key | sudo tee \
+  /usr/share/keyrings/jenkins-keyring.asc > /dev/null
+echo deb [signed-by=/usr/share/keyrings/jenkins-keyring.asc] \
+  https://pkg.jenkins.io/debian binary/ | sudo tee \
+  /etc/apt/sources.list.d/jenkins.list > /dev/null
 
 sudo apt-get update
 sudo apt-get install jenkins -y
